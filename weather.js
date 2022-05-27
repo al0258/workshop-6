9a3058f8b3c66b4a1fb96699c0e462d016af8eed
Amit Lago <amitlago@Amits-MacBook-Pro.local>
2022-05-27 12:36:00 +0300

    finish get-temp function

--- weather.js
+++ weather.js
@@ -10,26 +10,49 @@
 
 // Commander usage example for your reference:
 import chalk from "chalk";
+import dotenv from "dotenv";
 import { Command } from "commander";
+import fetch from "node-fetch";
 const program = new Command();
 
+dotenv.config();
+
+const API_KEY = process.env.API_KEY;
+
+const WEATHER_API_BASE_URL = process.env.WEATHER_API_BASE_URL;
+
+async function fetchWeatherInACity(cityName) {
+  try {
+    const requestString = new URLSearchParams({
+      q: cityName,
+      units: 'metric',
+      appId: API_KEY,
+    }).toString();
+    console.log(requestString);
+    const requestUrl = `${WEATHER_API_BASE_URL}/weather?${requestString}`;
+    const response = await fetch(requestUrl);
+    const weatherData = await response.json();
+
+    return weatherData;
+  }
+  catch (err) {
+    throw err;
+  }
+}
+
 program
-  .name("cli-calc")
-  .description("The best CLI calculator")
+  .name("Weather monday-u app")
+  .description("The best weather app in the whole wide worl, sorry, web")
   .version("1.0.0");
 
 program
-  .command("add")
-  .description("Add two numbers")
-  .argument("<number>", "first operand")
-  .argument("<number>", "second operand")
-  .option("-c, --color <string>", "Result color", "white")
-  .action((firstNumber, secondNumber, options) => {
-    console.log(
-      chalk[options.color](
-        `Result: ${Number(firstNumber) + Number(secondNumber)}`
-      )
-    );
+  .command("get-temp")
+  .description("Displays the temp of a selected city")
+  .argument("<string>", "city name")
+  .action (async (cityName) => {
+    const weatherData = await fetchWeatherInACity(cityName);
+    const { temp: temp } = weatherData.main;
+    console.log(`It's ${temp} degrees in ${cityName}`);
   });
 
 program.parse();
